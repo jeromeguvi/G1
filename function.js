27cf3a6f30f74247a18d534530d8e289451c4451
jeromeguvi <103776850+jeromeguvi@users.noreply.github.com>
2022-09-18 16:05:50 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,39 @@
+//function jerome(a,b) {
+  //  console.log(a+b);
+//}
+//jerome(3,5);
+
+//function jerome(a,b) {
+  //  return (a+b);
+//}
+//console.log(jerome(4,5));
+
+//let a = function () {
+  //  console.log("welcome")
+//}
+//a()
+
+//let a = function (name) {
+  //  console.log("welcome " + name)
+//}
+//a("jerome")
+
+//let a = function(){}------anonymos fn
+//let b = ()=>{}------arrow fn
+
+// IIFE 
+
+//()()
+
+//(()=>{
+  //  console.log("hi jerome")
+//})()
+
+//((name)=>{
+  //  console.log("hi jerome")
+    //console.log (name)
+//})("madurai")
+
+let b = (a)=>{
+    
+}
